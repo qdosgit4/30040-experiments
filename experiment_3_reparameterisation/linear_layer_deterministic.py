1e01f4e67bd0d62acb0d503bb6e940a2fb3b899f
qdosgit5 <qdosgit4+5@gmail.com>
2026-02-28 22:07:31 +0000

    Have isolated the issue to the log(1 + exp(rho)) line

--- experiment_3_reparameterisation/linear_layer_deterministic.py
+++ experiment_3_reparameterisation/linear_layer_deterministic.py
@@ -57,6 +57,9 @@ class Linear_deterministic(Module):
         self.rho_weight = Parameter(torch.empty(out_features,
                                                 in_features, **factory_kwargs))
 
+        self.sigma_weight = Parameter(torch.empty(out_features,
+                                                in_features, **factory_kwargs))
+
         self.register_buffer('eps_weight', torch.empty(out_features,
                                                         in_features,
                                                         **factory_kwargs),
@@ -70,6 +73,8 @@ class Linear_deterministic(Module):
         
             self.rho_bias = Parameter(torch.empty(out_features, **factory_kwargs))
             
+            self.sigma_bias = Parameter(torch.empty(out_features, **factory_kwargs))
+            
             self.register_buffer('eps_weight',
                              torch.empty(out_features, in_features, **factory_kwargs),
                                  persistent=False)
@@ -82,18 +87,14 @@ class Linear_deterministic(Module):
             
             self.register_parameter("rho_bias", None, **factory_kwargs)
             
+            self.register_parameter("sigma_bias", None, **factory_kwargs)
+            
             self.register_buffer('eps_weight', None, persistent=False)
 
         self.reset_parameters(mu_init, rho_init)
 
         
     def reset_parameters(self, mu_init: float, rho_init: float) -> None:
-        """
-        Resets parameters based on their initialization used in ``__init__``.
-        """
-        # Setting a=sqrt(5) in kaiming_uniform is the same as initializing with
-        # uniform(-1/sqrt(in_features), 1/sqrt(in_features)). For details, see
-        # https://github.com/pytorch/pytorch/issues/57109
         
         init.kaiming_uniform_(self.mu_weight, a=math.sqrt(5))
         
@@ -112,17 +113,27 @@ class Linear_deterministic(Module):
             
     def forward(self, input: Tensor) -> Tensor:
         
-        """
-        Runs the forward pass.
-        """
+        # self.sigma_weight = torch.log1p(torch.exp(self.rho_weight))
 
-        self.weight = self.mu_weight
+        # self.sigma_weight = torch.exp(self.rho_weight)
+
+        # input_identity = torch.ones_like(input)
+
+        # self.weight = self.mu_weight + self.rho_weight
+
+        # print(self.rho_weight)
+
+        # print(torch.exp(self.rho_weight))
         
-        return F.linear(input, self.weight, self.bias)
+        return F.linear(input,
+                        torch.log1p(self.rho_weight),
+                        self.bias)
 
     
     def extra_repr(self) -> str:
+        
         """
         Return the extra representation of the module.
         """
+        
         return f"in_features={self.in_features}, out_features={self.out_features}, bias={self.bias is not None}"
