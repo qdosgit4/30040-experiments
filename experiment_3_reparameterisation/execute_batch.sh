05f29cd356cbfd3bff3e39259747210c64cce0e1
qdosgit4 <244482937+qdosgit4@users.noreply.github.com>
2026-03-09 22:31:26 +0000

    Rewrote execute_batch

--- experiment_3_reparameterisation/execute_batch.sh
+++ experiment_3_reparameterisation/execute_batch.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+11;rgb:f6f6/f8f8/fafa
+mapfile -t files < <(ls -v *_batch_*)
 
-for file in *"_batch_"*; do [ -e "$file" ] && echo "Processing $file"; done
+for file in "${files[@]}"; do
+    
+    echo "$file"
+    
+done
