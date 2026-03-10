dd6f0c8df567cb5d89970c1632cb12e57d016ee3
qdosgit4 <244482937+qdosgit4@users.noreply.github.com>
2026-03-10 14:57:50 +0000

    Pi probabilities plotted for different quantities of training epochs

--- experiment_3_reparameterisation/experiment_reparam.py
+++ experiment_3_reparameterisation/experiment_reparam.py
@@ -186,21 +186,25 @@ def run_utilisation_loop(model: nn.Module, weights_path: str):
 
             plt.figure(figsize=(6, 4))
 
-            plt.plot(x, y, marker='o', linestyle='-', color='steelblue',
-                     label='Estimated PDF')
-
-            plt.fill_between(x, y, color='steelblue', alpha=0.2)
-
-            plt.xlabel('x')
-            plt.ylabel('Density')
-            plt.grid(True, which='both', ls='--', lw=0.5, alpha=0.7)
+            plt.scatter(
+                        x, y,
+                        color='black',          # point colour
+                        edgecolor='black',          # optional border around each marker
+                        s=60,                       # marker size
+                        label='Samples'
+            )
+            
+            plt.xlabel('Input value')
+            plt.ylabel('Probability of π classification')
+            plt.grid(True, ls='--', lw=0.5, alpha=0.7)
             plt.legend()
+            plt.tight_layout()
 
             plt.title('Probability Density Function')
 
             plt.tight_layout()
 
-            plt.savefig('plot.pdf', dpi=300)
+            plt.savefig('pi_probabilities_128_epoch.pdf', dpi=300)
             plt.close()      
 
 
